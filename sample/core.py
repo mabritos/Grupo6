16c9d91b03519e94ee3cc6abf361229c5fa38b1a
mabritos <mabritos@correo.um.edu.uy>
2020-10-03 21:40:45 -0300

    Implementado hasta dibujo de landmarks, faltan las comprobaciones de sueño

--- sample/core.py
+++ sample/core.py
@@ -1,3 +1,22 @@
+import cv2
+from face_detection import FaceDetection
+
+
 face_detection = FaceDetection()
+webcam = cv2.VideoCapture(0)
 
 while True:
+    # Obtenemos un nuevo frame
+    _, frame = webcam.read()
+
+    # Mandamos el frame a FaceDetection para analizarlo
+    face_detection.refresh(frame)
+
+    # Mostrar en el frame los landmarks
+    face_detection.draw_landmarks()
+
+    cv2.imshow("Frame", frame)
+    
+    if cv2.waitKey(1) == 27:
+        break
+
--- sample/face_detection.py
+++ sample/face_detection.py
@@ -1,15 +1,25 @@
+from __future__ import division
 from imutils import face_utils
 from scipy.spatial import distance as dist
 import cv2
 import dlib
+import numpy as np
 
 class FaceDetection(object):
     """Esta clase detecta un rostro y sus landmarks"""
 
     def __init__(self):
-        """Constructor, esto es lo que se ejecuta cuando se crea un objeto de la clase FaceDetection.
-            Se crean todos sus atributos y se setean en None por default"""
+        """Constructor, esto es lo que se ejecuta cuando se crea un objeto de la clase FaceDetection."""
+        
         self.frame = None
+        self.counter = 0
+        self.EYE_AR_THRESH = 0.2
+        self.EYE_AR_CONSEC_FRAMES = 30
+        self.YAWN_THRESH = 20
+        self.yawn_counter = 0
+        self.blink_verification = False
+        self.blink_counter = 0
+        self.t_end = 0 #Variable para controlar el tiempo de un bostezo
 
         #_face_detector detecta rostros
         self._face_detector = dlib.get_frontal_face_detector()
@@ -18,18 +28,20 @@ class FaceDetection(object):
         self._predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
     def _analyze(self):
-        """Detecta un rostro"""
+        """Detecta un rostro y todos sus landmarks"""
 
         frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
         faces = self._face_detector(frame)
-        
+
         try:
             landmarks = self._predictor(frame, faces[0])
             self.landmarks = face_utils.shape_to_np(landmarks)
+            self.final_ear(self.landmarks)
+            self.lips_distance = self.lip_distance(self.landmarks)
 
         except IndexError:
-            self.eye_left = None
-            self.eye_right = None
+            self.left_eye = None
+            self.right_eye = None
 
 
     def refresh(self, frame):
@@ -37,17 +49,21 @@ class FaceDetection(object):
 
         self.frame = frame
         self._analyze()
+        
+        
 
-
+    
     def final_ear(self, shape):
+        """ Setea los ojos y las orejas """
+
         (l_start, l_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
         (r_start, r_end) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
         self.left_eye = shape[l_start:l_end]
         self.right_eye = shape[r_start:r_end]
 
-        self.left_ear = eye_aspect_ratio(self.left_eye)
-        self.right_ear = eye_aspect_ratio(self.right_eye)
+        self.left_ear = self.eye_aspect_ratio(self.left_eye)
+        self.right_ear = self.eye_aspect_ratio(self.right_eye)
 
         self.ear = (self.left_ear + self.right_ear) / 2.0
 
@@ -59,4 +75,33 @@ class FaceDetection(object):
         ear = (A + B) / (2.0 * C)
         return ear
 
+
+    def lip_distance(self, shape):
+        """Calcula la distancia entre el labio superior e inferior"""
+
+        top_lip = shape[50:53]
+        top_lip = np.concatenate((top_lip, shape[61:64]))
+
+        low_lip = shape[56:59]
+        low_lip = np.concatenate((low_lip, shape[65:68]))
+
+        top_mean = np.mean(top_lip, axis=0)
+        low_mean = np.mean(low_lip, axis=0)
+
+        distance = abs(top_mean[1] - low_mean[1])
+        return distance
+
+
+    def draw_landmarks(self):
+        """Dibuja en el frame los landmarks"""
+
+        try:
+            left_eye_hull = cv2.convexHull(self.left_eye)
+            right_eye_hull = cv2.convexHull(self.right_eye)
+            cv2.drawContours(self.frame, [left_eye_hull], -1, (0, 255, 0), 1)
+            cv2.drawContours(self.frame, [right_eye_hull], -1, (0, 255, 0), 1)
+            lip = self.landmarks[48:60]
+            cv2.drawContours(self.frame, [lip], -1, (0, 255, 0), 1)
+        except:
+            print('cara perdida')
         
