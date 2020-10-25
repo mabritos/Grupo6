14d22ec473d2968f97e0a0ff513ce718a39fa258
mabritos <mabritos@correo.um.edu.uy>
2020-10-25 20:34:24 -0300

    encapsulacion de csv, arreglo de errores

--- sample/csv_handler.py
+++ sample/csv_handler.py
@@ -1,12 +1,15 @@
+import csv
+import cv2
+import base64
+from datetime import datetime
 
 class CsvHandler():
 
-    def csv_input(car_id,event,location,speed):
-            cv2.imwrite("frame.jpg", self.frame)
+    def csv_input(car_id,event,location,speed, frame):
+            cv2.imwrite("frame.jpg", frame)
             with open("frame.jpg", "rb") as imageFile:
                 image_aux = base64.b64encode(imageFile.read())
                 image = image_aux[2:]
             with open('Datos.csv','a') as f:
                 thewriter = csv.writer(f)
-                location = ''+str(self.gps.get_lat()) +', '+ str(self.gps.get_lon())
-                thewriter.writerow([car_id,datetime.today().strftime('%Y-%m-%d %H:%M:%S'),event,image,location,self.gps.get_speed()])
+                thewriter.writerow([car_id,datetime.today().strftime('%Y-%m-%d %H:%M:%S'),event,image,location,speed])
--- sample/face_detection.py
+++ sample/face_detection.py
@@ -7,8 +7,6 @@ import numpy as np
 import os
 import time
 import math
-import csv
-import base64
 from alarms import Alarms
 from datetime import datetime
 from csv_handler import CsvHandler
@@ -155,7 +153,7 @@ class FaceDetection(object):
             if ((self.counter <= time.time() - self.EYE_AR_CONSEC_FRAMES) and self.t_end_blink == 0 ):
                 self.t_end_blink = time.time() + 5
                 self.alarm.text_to_speech("El conductor se esta durmiendo")
-                CsvHandler.csv_input(self.CAR_REGISTRATION,self.EVENT_STRING_SLEEP,'Not found','35')
+                CsvHandler.csv_input(self.CAR_REGISTRATION,self.EVENT_STRING_SLEEP,''+str(self.gps.get_lat()) +', '+ str(self.gps.get_lon())+'',str(self.gps.get_speed()), self.frame)
                 
                 
 
@@ -188,7 +186,7 @@ class FaceDetection(object):
                 if len(self.blink_time_alert_counter_a) == self.BLINK_TIME_ALERT  :
                     self.blink_time_alert_counter_a = []
                     self.alarm.text_to_speech("usted presenta sintomas de sueno")
-                    CsvHandler.csv_input(self.CAR_REGISTRATION,self.EVENT_STRING_DROWSINESS,'Not found','60')
+                    CsvHandler.csv_input(self.CAR_REGISTRATION,self.EVENT_STRING_DROWSINESS,''+str(self.gps.get_lat()) +', '+ str(self.gps.get_lon()),str(self.gps.get_speed()), self.frame)
                     #self.alarm.yawn_alert()
         else:
             self.blink_counter_verification = True
